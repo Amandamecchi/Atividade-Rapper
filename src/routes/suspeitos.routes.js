1208aff579ee9296d2a0a432954d26cdccaefca6
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-10-10 09:38:42 -0300

    Feat: cadastro de suspeitos

---
+++ src/routes/suspeitos.routes.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+
+const suspeitoRoutes = Router();
+
+// Array com suspeito pré-cadastrados
+let suspeito = [
+  {
+    id: Math.floor(Math.random() * 1000000),
+    nome: "Beyonce",
+    idade: 43,
+    envolvimento: true,
+  },
+  {
+    id: Math.floor(Math.random() * 1000000),
+    nome: "will Smith",
+    idade: 56,
+    envolvimento: true,
+  },
+  {
+    id: Math.floor(Math.random() * 1000000),
+    nome: "Ariana Grande",
+    idade: 31,
+    envolvimento: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000000),
+    nome: "Kanye West",
+    idade: 47,
+    envolvimento: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000000),
+    nome: "Jay Z",
+    idade: 54,
+    envolvimento: true,
+  },
+];
+
